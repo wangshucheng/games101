22bf32e6fde624ccd3b124fc1f8132a8330dece1
ShushingWong <122181582@qq.com>
2021-08-22 11:18:16 +0800

    Microfacet

--- Homework7/Assignment7/Material.hpp
+++ Homework7/Assignment7/Material.hpp
@@ -7,7 +7,7 @@
 
 #include "Vector.hpp"
 
-enum MaterialType { DIFFUSE};
+enum MaterialType { DIFFUSE, Microfacet};
 
 class Material{
 private:
@@ -85,6 +85,41 @@ private:
         return a.x * B + a.y * C + a.z * N;
     }
 
+	float DistributionGGX(Vector3f N, Vector3f H, float roughness)
+	{
+		float a = roughness * roughness;
+		float a2 = a * a;
+		float NdotH = std::max(dotProduct(N, H), 0.0f);
+		float NdotH2 = NdotH * NdotH;
+
+		float nom = a2;
+		float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+		denom = M_PI * denom * denom;
+
+		return nom / std::max(denom, 0.0000001f); // prevent divide by zero for roughness=0.0 and NdotH=1.0
+	}
+
+	float GeometrySchlickGGX(float NdotV, float roughness)
+	{
+		float r = (roughness + 1.0);
+		float k = (r*r) / 8.0;
+
+		float nom = NdotV;
+		float denom = NdotV * (1.0 - k) + k;
+
+		return nom / denom;
+	}
+
+	float GeometrySmith(Vector3f N, Vector3f V, Vector3f L, float roughness)
+	{
+		float NdotV = std::max(dotProduct(N, V), 0.0f);
+		float NdotL = std::max(dotProduct(N, L), 0.0f);
+		float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+		float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+		return ggx1 * ggx2;
+	}
+
 public:
     MaterialType m_type;
     //Vector3f m_color;
@@ -142,6 +177,17 @@ Vector3f Material::sample(const Vector3f &wi, const Vector3f &N){
             
             break;
         }
+		case Microfacet:
+		{
+			// uniform sample on the hemisphere
+			float x_1 = get_random_float(), x_2 = get_random_float();
+			float z = std::fabs(1.0f - 2.0f * x_1);
+			float r = std::sqrt(1.0f - z * z), phi = 2 * M_PI * x_2;
+			Vector3f localRay(r*std::cos(phi), r*std::sin(phi), z);
+			return toWorld(localRay, N);
+
+			break;
+		}
     }
 }
 
@@ -156,6 +202,15 @@ float Material::pdf(const Vector3f &wi, const Vector3f &wo, const Vector3f &N){
                 return 0.0f;
             break;
         }
+		case Microfacet:
+		{
+			// uniform sample probability 1 / (2 * PI)
+			if (dotProduct(wo, N) > 0.0f)
+				return 0.5f / M_PI;
+			else
+				return 0.0f;
+			break;
+		}
     }
 }
 
@@ -172,6 +227,45 @@ Vector3f Material::eval(const Vector3f &wi, const Vector3f &wo, const Vector3f &
             else
                 return Vector3f(0.0f);
             break;
+        }
+        case Microfacet:
+        {
+			// Disney PBR 方案
+			float cosalpha = dotProduct(N, wo);
+			if (cosalpha > 0.0f) {
+				float roughness = 0.35;
+
+				Vector3f V = -wi;
+				Vector3f L = wo;
+				Vector3f H = normalize(V + L);
+
+				// 计算 distribution of normals: D
+				float D = DistributionGGX(N, H, roughness);
+
+				// 计算 shadowing masking term: G
+				float G = GeometrySmith(N, V, L, roughness);
+
+				// 计算 fresnel 系数: F
+				float F;
+				float etat = 1.85;
+				fresnel(wi, N, etat, F);
+
+				Vector3f nominator = D * G * F;
+				float denominator = 4 * std::max(dotProduct(N, V), 0.0f) * std::max(dotProduct(N, L), 0.0f);
+				Vector3f specular = nominator / std::max(denominator, 0.001f);
+
+				// 能量守恒
+				float ks_ = F;
+				float kd_ = 1.0f - ks_;
+
+				Vector3f diffuse = 1.0f / M_PI;
+
+				// 因为在 specular 项里已经考虑了折射部分的比例：F，所以折射部分不需要再乘以 ks_ （ks_ * Ks * specular）
+				return Ks * specular + kd_ * Kd * diffuse;
+			}
+			else
+				return Vector3f(0.0f);
+			break;
         }
     }
 }
--- Homework7/Assignment7/Sphere.hpp
+++ Homework7/Assignment7/Sphere.hpp
@@ -56,7 +56,7 @@ public:
         float t0, t1;
         if (!solveQuadratic(a, b, c, t0, t1)) return result;
         if (t0 < 0) t0 = t1;
-        if (t0 < 0) return result;
+        if (t0 < 0.5) return result;    // 圆的相交判定精度不够
         result.happened=true;
 
         result.coords = Vector3f(ray.origin + ray.direction * t0);
